e5359053f3d61cdd7ffd148f63f131d93a77a2f6
Melissa Canales <missmeliss621@gmail.com>
2016-04-15 10:09:28 -0500

    I answered the exercise questions as part of my education.

---
+++ if.php
@@ -0,0 +1,45 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+    // TODO: Replace the blanks with the correct description
+    echo "5 is less than 10\n";
+}
+
+if ($b > $a) {
+    // TODO: Replace the blanks with the correct description
+    echo "10 is greater than 5\n";
+}
+
+if ($b >= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "10 is greater than or equal to '10'\n";
+}
+
+if ($b <= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "10 is less than or equal to '10'\n";
+}
+
+if ($b == $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "10 is equal '10'\n";
+}
+
+if ($b === $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "10 is strictly data and type equal to '10'\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if (false) {
+    echo "10 is not equal to '10'\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if (false) {
+    echo "10 is not identical to '10'\n";
+}
