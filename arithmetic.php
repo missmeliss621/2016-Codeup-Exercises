21ac8a11160900c85d49e8e965b5c0c543e97462
Melissa Canales <missmeliss621@gmail.com>
2016-04-19 15:26:25 -0500

    more class work to this file. No big edits were made.

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function add($a, $b)
 {
     return $a + $b;
@@ -7,17 +8,26 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a*$b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    return $a/$b;
+}
+function modulus($a, $b){
+    return $a % $b;
 }
 
-// Add code to test your functions here
+echo add(25, 25) .PHP_EOL;
+echo subtract(25, 25) .PHP_EOL;
+echo multiply(25, 25) .PHP_EOL;
+echo divide(25, 25) .PHP_EOL;
+echo modulus(55, 7) .PHP_EOL;
+
+
